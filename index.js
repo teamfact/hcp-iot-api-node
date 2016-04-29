31a3889b9c0cd28bed6611a82272a43a1421c019
Mike Zaschka <mike.zaschka@gmx.de>
2016-04-29 23:21:14 +0200

    Added more MMS methods.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var HCPIoTServiceAPI = function () {
 }
 
 HCPIoTServiceAPI.RemoteDeviceManagementService = require('./lib/remote_device_managment_service.js');
-HCPIoTServiceAPI.MessageManagementService = require('./lib/message_managment_service.js');
+HCPIoTServiceAPI.MessageManagementService = require('./lib/message_management_service.js');
 
 module.exports = HCPIoTServiceAPI;
